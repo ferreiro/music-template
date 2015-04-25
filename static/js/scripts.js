ea3ab34f25d1dad5636a8fd7c97676b6661320cd
Jorge Ferreiro <jorgeWork@iMac-de-Jorge.local>
2015-04-25 21:14:48 +0200

    Initial commit, forked from my repo Basic-website-template

---
+++ static/js/scripts.js
@@ -0,0 +1,21 @@
+// TOBE IMPLEMENTED
+var mobileMenu = $('#js-navbar-menu'),
+	menu = $('.headerMenu'),
+	mobileSearch = $('#js-navbar-search'),
+	mobileSearchForm = $('.headerSearch'),
+	closeSearch = $('.headerSearchClose');
+
+mobileMenu.click(function() { 
+	mobileMenu.toggleClass('pressed');
+	menu.slideToggle("fast");
+});
+
+mobileSearch.click(function() {
+	menu.hide(0);
+	mobileMenu.removeClass('pressed');
+	mobileSearchForm.fadeToggle("fast");
+});
+
+closeSearch.click(function() {
+	mobileSearchForm.hide();
+});
